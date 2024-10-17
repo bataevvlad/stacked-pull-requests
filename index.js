9d2a367dd43a6372709484d7143ca30da5802514
bataevvlad <bataevvlad@gmail.com>
2024-10-17 14:24:09 +0200

    PR2: Add multiplication and division functions

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const math = require('./math');
 
 console.log("Addition (2 + 3):", math.add(2, 3));
 console.log("Subtraction (5 - 2):", math.subtract(5, 2));
+console.log("Multiplication (4 * 3):", math.multiply(4, 3));
+console.log("Division (10 / 2):", math.divide(10, 2));
